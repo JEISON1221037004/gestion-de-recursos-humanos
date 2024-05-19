7eddc71130fdaf50be7fdfcbd74f8943316d1572
Jeison gonzalez <jeisonmanuyama@gmail.com>
2024-05-19 18:59:33 -0500

    listado departamentos

---
+++ app/Http/Controllers/DepartamentoController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Departamento;
+use Illuminate\Http\Request;
+
+class DepartamentoController extends Controller
+{
+    public function index()
+    {
+        $departamentos = Departamento::all();
+        return view('departamentos.index', compact('departamentos'));
+    }
+
+    public function create()
+    {
+        return view('departamentos.new');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+            'ubicacion' => 'required|string|max:255',
+        ]);
+
+        Departamento::create($request->all());
+        return redirect()->route('departamentos.index');
+    }
+
+    public function edit(Departamento $departamento)
+    {
+        return view('departamentos.edit', compact('departamento'));
+    }
+
+    public function update(Request $request, Departamento $departamento)
+    {
+        $request->validate([
+            'nombre' => 'string|max:255',
+            'ubicacion' => 'string|max:255',
+        ]);
+
+        $departamento->update($request->all());
+        return redirect()->route('departamentos.index');
+    }
+
+    public function destroy(Departamento $departamento)
+    {
+        $departamento->delete();
+        return redirect()->route('departamentos.index');
+    }
+}
