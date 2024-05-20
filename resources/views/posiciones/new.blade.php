68c0cc59544ca74a57be05addf9349d08cc24c95
Jeison gonzalez <jeisonmanuyama@gmail.com>
2024-05-19 19:33:48 -0500

    agrgar posicion

---
+++ resources/views/posiciones/new.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Agregar Nueva Posición</title>
+</head>
+<body>
+<div class="container">
+    <h1>Agregar Nueva Posición</h1>
+    <form action="{{ route('posiciones.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="nombre" class="form-label">Nombre</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" required>
+        </div>
+        <div class="mb-3">
+            <label for="descripcion" class="form-label">Descripción</label>
+            <textarea class="form-control" id="descripcion" name="descripcion" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Crear</button>
+        <a href="{{ route('posiciones.index') }}" class="btn btn-warning">Cancelar</a>
+    </form>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
