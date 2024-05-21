84aa1c36c232c1a5af8cc9ded466b4280842c24c
JohanStiven8310 <jhanstivn93@gmail.com>
2024-05-21 10:56:13 -0500

    DEFINICION DE LA MIGRACION "EMPLOYEES"

--- database/migrations/2024_05_21_155017_create_employees_table.php
+++ database/migrations/2024_05_21_155017_create_employees_table.php
@@ -9,13 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('employees', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('employees', function (Blueprint $table) {
+        $table->id();
+        $table->string('first_name');
+        $table->string('last_name');
+        $table->string('email')->unique();
+        $table->unsignedBigInteger('position_id');
+        $table->timestamps();
+
+        $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
+    });
+}
+
 
     /**
      * Reverse the migrations.
