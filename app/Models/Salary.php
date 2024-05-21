2e93c6e41ca00202115cc3ab0a656ef14c6eb429
JohanStiven8310 <jhanstivn93@gmail.com>
2024-05-21 11:03:01 -0500

    modelo salary

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salary extends Model
 {
-    use HasFactory;
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
